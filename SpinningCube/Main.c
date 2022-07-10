b1ac0b00ddea097f1c313b23203242c85e88547f
Keith Holman <holmak@gmail.com>
2022-07-10 11:12:29 -0700

    Dynamically load later GL functions, and register a GL error handler.

--- SpinningCube/Main.c
+++ SpinningCube/Main.c
@@ -1,11 +1,14 @@
 #include <stdbool.h>
 #include <stdint.h>
-#include "SDL.h"
+#include <stdio.h>
 #include "SDL/SDL.h"
+#include "GL.h"
 
 #pragma comment(lib, "SDL2main")
 #pragma comment(lib, "SDL2")
 #pragma comment(lib, "opengl32")
+
+#define DEBUG_GRAPHICS true
 
 void Check(bool condition)
 {
@@ -15,12 +18,47 @@ void Check(bool condition)
     }
 }
 
+void OnGLDebugMessage(GLenum source, GLenum type, unsigned id, GLenum severity,
+    GLsizei length, const char *message, const void *userParam)
+{
+    char *sourceName = "unknown";
+    char *typeName = "unknown";
+    char *severityName = "unknown";
+
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API: sourceName = "API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM: sourceName = "window system"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: sourceName = "shader compiler"; break;
+    }
+
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR: typeName = "error";
+    }
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH: severityName = "high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM: severityName = "medium"; break;
+    case GL_DEBUG_SEVERITY_LOW: severityName = "low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: severityName = "notice"; break;
+    }
+
+    fprintf(stderr, "GL error: %s, %s, %s, %s\n", sourceName, typeName, severityName, message);
+}
+
 int main(int argc, char *argv[])
 {
     Check(SDL_Init(SDL_INIT_EVERYTHING) == 0);
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    if (DEBUG_GRAPHICS)
+    {
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+    }
 
     SDL_Window *window = SDL_CreateWindow(
         "Screensaver",
@@ -29,7 +67,19 @@ int main(int argc, char *argv[])
     Check(window != NULL);
 
     SDL_GLContext context = SDL_GL_CreateContext(window);
+    Check(context != 0);
+    LoadGL();
     Check(SDL_GL_SetSwapInterval(-1) == 0);
+
+    if (DEBUG_GRAPHICS)
+    {
+        oglEnable(GL_DEBUG_OUTPUT);
+        oglEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        oglDebugMessageCallback(OnGLDebugMessage, NULL);
+        oglDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+    }
+
+    oglEnable(GL_TEXTURE0);
 
     for (;;)
     {
@@ -42,24 +92,19 @@ int main(int argc, char *argv[])
             }
         }
 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
+        oglClearColor(0.5f, 0.5f, 1.0f, 0.0f);
+        oglClear(GL_COLOR_BUFFER_BIT);
 
-        glClearColor(0.5f, 0.5f, 1.0f, 0.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_QUADS);
-        glColor3f(1, 0, 0);
-        glVertex2f(-0.5f, -0.5f);
-        glColor3f(0, 1, 0);
-        glVertex2f(0.5f, -0.5f);
-        glColor3f(0, 0, 1);
-        glVertex2f(0.5f, 0.5f);
-        glColor3f(1, 1, 0);
-        glVertex2f(-0.5f, 0.5f);
-        glEnd();
+        //glBegin(GL_QUADS);
+        //glColor3f(1, 0, 0);
+        //glVertex2f(-0.5f, -0.5f);
+        //glColor3f(0, 1, 0);
+        //glVertex2f(0.5f, -0.5f);
+        //glColor3f(0, 0, 1);
+        //glVertex2f(0.5f, 0.5f);
+        //glColor3f(1, 1, 0);
+        //glVertex2f(-0.5f, 0.5f);
+        //glEnd();
 
         SDL_GL_SwapWindow(window);
     }
