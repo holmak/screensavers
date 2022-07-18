6eeda7f2c802e32daa5fb377565dc10ab9255df5
Keith Holman <holmak@gmail.com>
2022-07-17 20:56:46 -0700

    Add requirements for the GL context: hardware acceleration, depth/stencil precision, sRGB.

--- SpinningCube/Main.c
+++ SpinningCube/Main.c
@@ -269,6 +269,10 @@ int main(int argc, char *argv[])
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
 
     if (DEBUG_GRAPHICS)
     {
